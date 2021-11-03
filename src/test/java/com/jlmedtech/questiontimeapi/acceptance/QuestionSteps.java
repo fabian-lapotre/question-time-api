9c17a05903b9d53074951d3b34f5214f5d882459
fabian <lapotrefabian@gmail.com>
2021-11-03 19:16:18 +0100

    feat(cucumber): install dependancy and create cucumber test skeleton

---
+++ src/test/java/com/jlmedtech/questiontimeapi/acceptance/QuestionSteps.java
@@ -0,0 +1,35 @@
+package com.jlmedtech.questiontimeapi.acceptance;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class QuestionSteps {
+
+        @Given("student ask for question")
+        public void student_ask_for_question(){
+        }
+
+        @When("student as no answer question")
+        public void student_as_no_answer_question(){
+
+        }
+
+        @Then("give him the question")
+        public void give_him_the_question(){
+
+        }
+
+        @When("student as already answer question")
+        public void student_as_already_answer_question(){
+
+        }
+
+        @Then("give him is answers")
+        public void give_him_is_answers(){
+
+        }
+
+
+
+}
